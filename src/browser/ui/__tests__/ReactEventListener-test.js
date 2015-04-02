0323d6fc19240650f3f27f6993f1e6a77ffc244f
Mikhail Davydov <mdavydov@productivemobile.com>
2015-04-02 18:58:44 +0200

    ReactEventListener.dispatchEvent ignores non own documents.

--- src/browser/ui/__tests__/ReactEventListener-test.js
+++ src/browser/ui/__tests__/ReactEventListener-test.js
@@ -184,4 +184,16 @@ describe('ReactEventListener', function() {
     expect(calls.length).toBe(1);
     expect(calls[0][EVENT_TARGET_PARAM]).toBe(instance.getInner().getDOMNode());
   });
+
+  it('should not dispatch events coming from non own elements', function() {
+    var callback = ReactEventListener.dispatchEvent.bind(null, 'test');
+    callback({
+      target: {
+        ownerDocument: {}
+      }
+    });
+
+    var calls = handleTopLevel.mock.calls;
+    expect(calls.length).toBe(0);
+  });
 });
